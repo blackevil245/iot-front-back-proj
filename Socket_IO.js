a18c41182b4d2725c29238c7ae58e23d9b6a4a98
kiefah <kiefah.harry@mail.bcu.ac.uk>
2017-08-17 11:33:05 +0300

    updated socket io file

---
+++ Socket_IO.js
@@ -0,0 +1,19 @@
+socket.on('NEW_LIGHT_DATA', function (data) {
+	$("#light_value").html(data);
+    log(message, {prepend: true});
+  });
+
+socket.on('NEW_HEAT_DATA', function (data) {
+	$("#temp_value").html(data);
+    log(message, {prepend: true});
+  });
+
+$('#fan_switch').change(function () {
+    if ($(this).is(':checked')) {
+		socket.emit('NEW_FAN_STATE', 1);
+        console.log($(this).val() + ' is now checked');
+    } else {
+        socket.emit('NEW_FAN_STATE', 0);
+		console.log($(this).val() + ' is now unchecked');
+    }
+});
