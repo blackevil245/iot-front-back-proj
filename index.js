fb66f5fb27fc91ff1361e539ab57a61a46db97b4
Hieu Nguyen <blackevil245@gmail.com>
2017-08-18 10:29:20 +0300

    Prepare for presentation

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const Readline = SerialPort.parsers.Readline;
 const parser = new Readline({ delimiter: '\r\n' });
 const customEmitter = require('./CustomEmitter');
 const socketServer = require('./server');
+const timers = require('timers')
 
 const {
   FAN_ON,
@@ -30,16 +31,12 @@ const arduinoPort = new SerialPort(PORT_NAME, {
 });
 
 function writeArduinoMessage(message) {
-  return new Promise((resolve, reject) => {
-    arduinoPort.write(Buffer.from(message), function(err) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve('Successfully write Arduino message', message);
-      }
-    });
-  })
-  .catch(err => console.error(err));
+  arduinoPort.write(Buffer.from(message), function(err) {
+    if (err) {
+      console.log('Error writting Arduino port ', arduinoPort)
+      throw err;
+    }
+  });
 }
 
 function startArduinoConnection() {
@@ -101,27 +98,27 @@ try {
   customEmitter.on(NEW_HEAT_DATA, heatData => {
     // Emit for socket regardless of control mode
     socketServer.emit(NEW_HEAT_DATA, heatData);
-    if (heatData >= 28) {
-      console.log('Opening window and fan');
-      writeArduinoMessage(WINDOW_OPEN);
-      writeArduinoMessage(FAN_ON);
-    } else {
-      console.log('Closing window and fan')
-      writeArduinoMessage(WINDOW_CLOSED);
-      writeArduinoMessage(FAN_OFF);
-    }
+    // if (heatData >= 27) {
+    //   console.log('Opening window');
+    //   writeArduinoMessage(WINDOW_OPEN);
+    //   writeArduinoMessage(FAN_ON);
+    // } else {
+    //   console.log('Closing window')
+    //   writeArduinoMessage(WINDOW_CLOSED);
+    //   writeArduinoMessage(FAN_OFF);
+    // }
   });
 
   customEmitter.on(NEW_LIGHT_DATA, lightData => {
     // Emit for socket regardless of control mode
     socketServer.emit(NEW_LIGHT_DATA, lightData);
-    if (lightData < 100) {
-      console.log('Turning on light');
-      writeArduinoMessage(LIGHT_ON);
-    } else {
-      console.log('Turning off light');
-      writeArduinoMessage(LIGHT_OFF);
-    }
+    // if (lightData < 100) {
+    //   console.log('Turning on light');
+    //   writeArduinoMessage(LIGHT_ON);
+    // } else {
+    //   console.log('Turning off light');
+    //   writeArduinoMessage(LIGHT_OFF);
+    // }
   });
 
   customEmitter.on(NEW_LED_DATA, ledData => {
